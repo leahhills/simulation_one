37b8bbe39b250aab589941c0c9fdeabc683d8590
chrisjxn <usjacksons@gmail.com>
2017-10-13 13:46:01 -0600

    created server folder and relocated server file

--- server/index.js
+++ server/index.js
@@ -4,11 +4,12 @@ const cors = require('cors');
 const massive = require('massive');
 require('dotenv').config();
 
+
 const app = express();
 app.use(bodyParser.json());
 app.use(cors());
-massive(process.env.CONNECTION_STRING).then(dbInstance=>app.set('db',dbInstance));
+massive(process.env.CONNECTION_STRING).then(dbInstance => app.set('db', dbInstance));
 
 
 const port = process.env.PORT || 3000;
-app.listen(port,()=>{console.log( `Server is listening on port ${port}.`) });
+app.listen(port, () => { console.log(`Server is listening on port ${port}.`) });
