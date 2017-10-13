5a87f8d121f6951cf72ff09ab942618ebb78d7fb
chrisjxn <usjacksons@gmail.com>
2017-10-13 15:30:57 -0600

    more server work

--- server/index.js
+++ server/index.js
@@ -3,12 +3,15 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const massive = require('massive');
 require('dotenv').config();
-
+const controller = require('./controller');
 
 const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 massive(process.env.CONNECTION_STRING).then(dbInstance => app.set('db', dbInstance));
+
+app.get('/api/bin/:id', controller.getBin);
+
 
 
 const port = process.env.PORT || 3000;
